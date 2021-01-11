823dbf54fcf0f0568b4866037ffa8e06ffd31569
Patti85 <pbeier24@googlemail.com>
2021-01-11 23:07:18 +0100

    add static images

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 <main class="wrap">
-    <section class="content-area content-thin">
+    <section class="content-area content-full-width">
         <h1>Aktuelles</h1>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article class="article-loop">
                     <header>
                         <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                     </header>
-                    <?php 
-                    add_image_size( 'single-feature', 400, 300, true );
-                    the_post_thumbnail('single-feature'); 
-                    
+                    <?php
+                    add_image_size('single-feature', 400, 300, true);
+                    the_post_thumbnail('single-feature');
+
                     ?>
                     <?php the_excerpt(); ?>
                 </article>
@@ -20,6 +20,26 @@
                 <p>Entschuldigung, hier steht noch nichts ...</p>
             </article>
         <?php endif; ?>
+        <h1>Unser Tierheim</h1>
+        <h1>Adoptieren und Helfen</h1>
+        <div class="animal-div">
+            <div class="animal-content-div">
+                <h2>Unsere Hunde</h2>
+                <img src="<?php echo get_template_directory_uri() ?>/images/hund.jpeg" alt="Unsere Hunde">
+            </div>
+            <div class="animal-content-div">
+                <h2>Unsere Katzen</h2>
+                <img src="<?php echo get_template_directory_uri() ?>/images/katze.jpeg" alt="Unsere Katzen">
+
+            </div>
+            <div class="animal-content-div">
+                <h2>Unsere Kleintiere</h2>
+                <img src="<?php echo get_template_directory_uri() ?>/images/kaninchen.jpeg" alt="Unsere Kleintiere">
+
+            </div>
+        </div>
     </section><?php get_sidebar(); ?>
+
 </main>
+
 <?php get_footer(); ?>
