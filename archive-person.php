a3e118bcf738fe93fba477e5f82d51553a22eb74
Patti85 <pbeier24@googlemail.com>
2021-01-31 10:57:23 +0100

    person type

---
+++ archive-person.php
@@ -0,0 +1,27 @@
+<?php
+get_header(); ?>
+
+<div class="main-wrap" role="main">
+
+    <section id="person-listing">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article class="article-full">
+                    <header>
+                        <h2><?php the_title(); ?></h2>
+                        By: <?php the_author(); ?>
+                    </header>
+                    <div class="column"">
+	       <?php get_template_part('template-parts/person-content', get_post_format()); ?>
+	    </div>
+                </article>
+            <?php endwhile;
+        else : ?>
+            <article>
+                <p>Leider gibt es noch keine Seiten ...</p>
+            </article>
+        <?php endif; ?>
+    </section>
+
+</div>
+
+<?php get_footer(); ?>
---
+++ single-person.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class() ?> id="person-<?php the_ID(); ?>">
+        <?php
+        $prename = get_post_meta($post->ID, 'prename', true);
+        $lastname = get_post_meta($post->ID, 'lastname', true); ?>
+        <div class="row column" id="animal-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </div>
+        <div class="main-wrap" role="main">
+            <div class="entry-content">
+                <div class="row">
+                    <div class="medium-6 columns">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <img class="animal-detail-image" src="<?php the_post_thumbnail_url('full'); ?>" />
+                        <?php endif; ?>
+                    </div>
+                    <div>
+                        <ul>
+                            <li class="prename">
+                                <?php echo $prename; ?>
+                            </li>
+                            <li class="lastname">
+                                <?php echo $lastname; ?>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row columns">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </article>
+<?php endwhile; ?>
+
+<?php get_footer();
