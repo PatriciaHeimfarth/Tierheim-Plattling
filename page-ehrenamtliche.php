6b308bdcf93265af067b7b7e5ede06a919e31fe3
Patti85 <pbeier24@googlemail.com>
2021-01-31 12:45:22 +0100

    ehrenamtliche

---
+++ page-ehrenamtliche.php
@@ -0,0 +1,41 @@
+<?php
+get_header();
+?>
+<div class="main-wrap" role="main">
+    <section id="animal-listing">
+        <?php $args = array(
+            'post_type' => 'person',
+            'post_status' => 'publish',
+            'posts_per_page' => 1000,
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'meta_key' => 'status',
+            'meta_value' => 'unsalaried'
+        );
+
+        $loop = new WP_Query($args);
+
+        while ($loop->have_posts()) : $loop->the_post();
+            if (has_post_thumbnail()) {     ?>
+                <div class="animal-list-format-div">
+                    <div class="infos-above-animal-image-div"><?php the_title(); ?></div>
+                    <a title="<?php the_title_attribute(); ?>" href="<?php the_permalink(); ?>">
+                        <?php the_post_thumbnail('full'); ?>
+                    </a>
+                    <div class="infos-below-animal-image-div">
+
+                        <?php
+                        ?>
+
+                    </div>
+                </div>
+        <?php
+            }
+
+        endwhile;
+
+        ?>
+    </section>
+
+</div>
+<?php get_footer() ?>
