25a7f0171a8ddc076c3c2b1600af5a3dcaa0d987
Patricia Heimfarth <pbeier24@googlemail.com>
2021-04-05 13:18:55 +0200

    adding missed animal

--- page-vermisstes-tier-melden.php
+++ page-vermisstes-tier-melden.php
@@ -2,28 +2,37 @@
 
 $postTitleError = '';
 
-if (isset($_POST['submitted']) && isset( $_POST['post_nonce_field'] )
- && wp_verify_nonce( $_POST['post_nonce_field'], 'post_nonce' )) {
+if (isset($_POST['submitted'])) {
 
     if (trim($_POST['name']) === '') {
         $postTitleError = 'Bitte einen Namen eingeben.';
         $hasError = true;
     }
 }
-$missing_animal = array(
-    'post_title' => wp_strip_all_tags($_POST['name']),
-    'post_type' => 'missing',
-    'post_status' => 'pending'
-);
+if (isset($_POST["name"])) {
+    $missing_animal = array(
+        'post_title' => wp_strip_all_tags($_POST['name']),
+        'post_content' => 'test',
+        'post_type' => 'missing',
+        'post_status' => 'pending',
+        'meta_input' => array(
+            'name' => wp_strip_all_tags($_POST['name'])
+        )
+    );
+    $post_id = wp_insert_post($missing_animal);
+    add_post_meta($post_id, 'name', $_POST['name'], false);
+    
+} else {
+    $missing_animal = null;
+}
 
-$post_id = wp_insert_post($missing_animal);
 ?>
 <?php
 get_header();
 ?>
 <div class="main-wrap" role="main">
 
-    <form action="" id="MissingForm" method="POST">
+    <form action="" name="new_missing" id="MissingForm" method="POST">
 
         <fieldset>
             <label for="name"><?php _e('Missing Name:', 'framework') ?></label>
@@ -32,22 +41,12 @@ get_header();
 
 
         <fieldset>
-            <input type="hidden" name="submitted" id="submitted" value="true" />
-            <?php wp_nonce_field('post_nonce', 'post_nonce_field'); ?>
+            <input type="hidden" name="submitted" name="action" value="true" />
             <button type="submit"><?php _e('Add Missing', 'framework') ?></button>
         </fieldset>
 
     </form>
 
 </div>
-<?php if ($postTitleError != '') { ?>
-    <span class="error"><?php echo $postTitleError; ?></span>
-    <div class="clearfix"></div>
-<?php } 
-if ( $post_id ) {
-    wp_redirect( home_url() );
-    exit;
-}
-?>
 
 <?php get_footer() ?>
