d03d8b609cec4bdc080e75d0e2e2b1a7f5668230
Patricia Heimfarth <pbeier24@googlemail.com>
2021-04-24 22:28:16 +0200

    center h1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main class="wrap">
     <section class="content-area content-full-width">
-        <h1>Aktuelles</h1>
+        <h1 class="text-center">Aktuelles</h1>
         <div class="container">
             <div class="row">
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
