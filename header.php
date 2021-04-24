ff8aa4c1e6316145368535ddcaea4cf820fb32f6
Patricia Heimfarth <pbeier24@googlemail.com>
2021-04-24 19:59:09 +0200

    replaceing css with bootstrap

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <title><?php bloginfo('name'); ?> &raquo; <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,84 +12,49 @@
   <header class="main-header">
     <h1><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
   </header>
-  <div class="navbar">
-    <div class="dropdown">
-      <button class="dropbtn" onclick="location.href='<?php echo esc_url(home_url('/')); ?>'">Home
-        <i class="fa fa-caret-down"></i>
-      </button>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Vermittlung
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/allgemeines">Allgemeines</a>
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/hunde">Hunde</a>
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/katzen">Katzen</a>
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/kleintiere">Kleintiere</a>
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/von-privat">Von Privat</a>
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/schutzgebuehr">Schutzgebühr</a>
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/ablauf-der-vermittlung">Ablauf der Vermittlung</a>
+            <a class="dropdown-item" href="<?php echo get_bloginfo('url') ?>/tiere-im-glueck">Tiere im Glück</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
     </div>
-    <div class="dropdown">
-      <button class="dropbtn">Vermittlung
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href= "<?php echo get_bloginfo('url') ?>/allgemeines">Allgemeines</a>
-        <a href="<?php echo get_bloginfo('url') ?>/hunde">Hunde</a>
-        <a href="<?php echo get_bloginfo('url') ?>/katzen">Katzen</a>
-        <a href="<?php echo get_bloginfo('url') ?>/kleintiere">Kleintiere</a>
-        <a href="<?php echo get_bloginfo('url') ?>/von-privat">Von Privat</a>
-        <a href="<?php echo get_bloginfo('url') ?>/schutzgebuehr">Schutzgebühr</a>
-        <a href="<?php echo get_bloginfo('url') ?>/ablauf-der-vermittlung">Ablauf der Vermittlung</a>
-        <a href="<?php echo get_bloginfo('url') ?>/tiere-im-glueck">Tiere im Glück</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn">Vermisst und Gefunden
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href="<?php echo get_bloginfo('url') ?>/fundtiere">Fundtiere</a>
-        <a href="<?php echo get_bloginfo('url') ?>/vermisste-tiere">Vermisste Tiere</a>
-        <a href="<?php echo get_bloginfo('url') ?>/vermisstes-tier-melden">Vermisstes Tier melden</a>
-        <a href="<?php echo get_bloginfo('url') ?>/datenschutz">Datenschutz</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn">Tierheim
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href="<?php echo get_bloginfo('url') ?>/der-verein">Der Verein</a>
-        <a href="<?php echo get_bloginfo('url') ?>/mitarbeiter">Unser Team</a>
-        <a href="<?php echo get_bloginfo('url') ?>/der-vorstand">Der Vorstand</a>
-        <a href="<?php echo get_bloginfo('url') ?>/ehrenamtliche">Unsere Ehrenamtlichen</a>
-        <a href="<?php echo get_bloginfo('url') ?>/gassigehen">Gassigehen</a>
-        <a href="<?php echo get_bloginfo('url') ?>/patenschaften">Patenschaften</a>
-        <a href="<?php echo get_bloginfo('url') ?>/jugendgruppe">Jugendgruppe</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn">Spenden und Helfen
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href="<?php echo get_bloginfo('url') ?>/mitglied-werden">Mitglied werden</a>
-        <a href="<?php echo get_bloginfo('url') ?>/spenden">Spenden</a>
-        <a href="<?php echo get_bloginfo('url') ?>/wunschzettel">Wunschliste Amazon und mehr</a>
-        <a href="<?php echo get_bloginfo('url') ?>/patenschaften">Patenschaften</a>
-        <a href="<?php echo get_bloginfo('url') ?>/ehrenamtlich-helfen">Ehrenamtlich helfen</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn">Kontakt
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href="<?php echo get_bloginfo('url') ?>/oeffnungszeiten">Öffnungszeiten</a>
-        <a href="<?php echo get_bloginfo('url') ?>/anfahrtskizze">Anfahrtskizze</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn" onclick="location.href='<?php echo get_bloginfo('url') ?>/impressum'">Impressum
-        <i class="fa fa-caret-down"></i>
-      </button>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn" onclick="location.href='<?php echo get_bloginfo('url') ?>/datenschutz'">Datenschutzerklärung
-        <i class="fa fa-caret-down"></i>
-      </button>
-    </div>
-  </div>
+  </nav>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
   <br>
   <br>
   <br>
